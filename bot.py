938530c69f9119ec3e144cd4377970b0614b1e49
Михаил Степанов <mir10102@mail.ru>
2022-05-14 21:18:52 +0300

    video cat recognizing completed

--- bot.py
+++ bot.py
@@ -37,7 +37,33 @@ def take_photo(message):
     cv2.imwrite(path+nameFile, det.outlineCatsOnImage(file.content, path+'best.pt'))
     bot.send_photo(message.chat.id, open(path+nameFile, 'rb'))
     print('sended')
-    #TODO: сделать название класса, вывод вероятности, опознование на видео и на несколько отправленных фото, удаление temp файлов после Nого temp, скачивание и распознавание в видео-кружочках
+    # TODO: сделать название класса, вывод вероятности, опознование на видео и на несколько отправленных фото, удаление temp файлов после Nого temp, скачивание и распознавание в видео-кружочках
+
+@bot.message_handler(content_types='video')
+def take_video(message):
+    file_name = message.json['video']['file_name']
+    file_info = bot.get_file(message.video.file_id)
+
+    download_name = f'temp_video/{file_name}'
+    out_name = f'video/{file_name}'
+
+    with open(path + download_name, 'wb') as output_file:
+        file_content = bot.download_file(file_info.file_path)
+        output_file.write(file_content)
+    bot.reply_to(message, f"Видео {file_name} сохранено")
+    
+    bot.send_message(message.chat.id, 'Please, wait...')
+    file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(TOKEN, file_info.file_path))
+
+    print(file)
+
+    #det.outlineCatsOnVideo(path+download_name, path+'best.pt', path + out_name)
+    det.outlineCatsOnVideo(path + download_name, path+'best.pt', path + out_name)
+    bot.send_video(message.chat.id, open(path+out_name, 'rb'))
+    #bot.send_video(message.chat.id, open(path+nameFile, 'rb'))
+    print('sended')
+    # TODO: сделать название класса, вывод вероятности, опознование на видео и на несколько отправленных фото, 
+    # удаление temp файлов после Nого temp, скачивание и распознавание в видео-кружочках
 
 if __name__ == "__main__":
     # Запускаем бота
